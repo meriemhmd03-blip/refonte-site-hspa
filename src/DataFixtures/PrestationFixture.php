834df33ab716588ee0e3d652441e9e8058eca84e
meriemhmd03-blip <meriemhmd.03@gmail.com>
2026-07-08 16:50:17 +0200

    persist/flush

--- src/DataFixtures/PrestationFixture.php
+++ src/DataFixtures/PrestationFixture.php
@@ -10,8 +10,26 @@ class PrestationFixture extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        $prestation = new Prestation(); //Je crée un objet Prestation
-        // $manager->persist($product);
+        $prestation = new Prestation();
+
+        $prestation->setNom('Head Spa');
+        $prestation->setSlug('head-spa');
+
+        $prestation->setHeroSousTitre('Soin du cuir chevelu japonais');
+
+        $prestation->setHeroImage('hero-headspa.webp');
+
+        $prestation->setIntroduction("Découvrez le Head Spa, un rituel inspiré des traditions japonaises qui allie détente profonde, massage du cuir chevelu et soins capillaires pour une expérience de bien-être unique.");
+
+        $prestation->setTechnique("Le soin associe massage du cuir chevelu, techniques de relaxation, nettoyage profond et soins adaptés afin d'améliorer le bien-être et la santé du cuir chevelu.");
+
+        $prestation->setImageTechnique('technique-headspa.webp');
+
+        $prestation->setResultats("À l'issue du soin, le cuir chevelu est assaini, les tensions sont relâchées et les cheveux retrouvent douceur, légèreté et brillance.");
+
+        $prestation->setImageResultats('resultat-headspa.webp');
+
+        $manager->persist($prestation);
 
         $manager->flush();
     }
